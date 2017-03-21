69ac58ae69474ec66e1fb6f1469fef4bd1f1e79b
AmFlint <amasselot@hotmail.com>
2017-03-21 10:29:47 +0100

    configuration connexion

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+// Définition des paramètres qui vont être utilisé pour la connection à la BDD.
+
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "root");
+define("DB_NAME", "medical");
+
+// Définition des PATH principaux
+
+define("ROOT_PATH", "/medical/");
+define("ROOT_URL", "http://localhost:8888/medical/");
