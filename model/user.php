a6ab33092e0029920b30768f4ad106350ebb59ca
AmFlint <amasselot@hotmail.com>
2017-03-21 16:08:09 +0100

    verif compte deja existant creation

--- model/user.php
+++ model/user.php
@@ -3,11 +3,13 @@
 class UserModel extends Model
 {
     public function register()
-
     {
-        $password = md5($_POST['password']);
+        $password = hash("sha512",$_POST['password']);
 
-        if ($_POST['submit']) {
+        if ($_POST['submit'] && $this->exists($_POST['mail'])) {
+            echo 'Vous avez déjà un compte';
+            return;
+        } else if ($_POST['submit']) {
             $sql = "INSERT INTO `users` (`mail`,`password`, `universite`) VALUES (:mail, :password , :universite)";
 
             $stmt = $this->_db->prepare($sql);
@@ -17,14 +19,20 @@ class UserModel extends Model
             $stmt->bindValue(':universite',$_POST['universite']);
 
             $stmt->execute();
-
         }
             return;
-
     }
 
     public function login()
     {
 
+    }
+
+    private function exists($info)
+    {
+        $stmt = $this->_db->prepare('SELECT COUNT(*) FROM `users` WHERE `mail` = :mail');
+        $stmt->bindValue(":mail", $info);
+        $stmt->execute();
+        return (bool) $stmt->fetchColumn();
     }
 }
