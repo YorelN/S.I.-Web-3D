f0a4e37a5d375715576d897d80c15e3dd1ad0be3
AmFlint <amasselot@hotmail.com>
2017-03-21 14:39:29 +0100

    les images et view du login

---
+++ views/users/login.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../assets/styles/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+
+    <title>Login</title>
+</head>
+<body id="login">
+<main>
+    <div class="login">
+        <div class="login-screen">
+            <div class="app-title">
+                <a class="blue_btn" href="#">Inscription</a>
+            </div>
+            <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
+                <div class="login-form">
+                    <div class="control-group">
+                        <input type="text" value="" placeholder="E-mail" name="mail">
+                    </div>
+
+                    <div class="control-group">
+                        <input type="password" class="login-field" value="" placeholder="Mot de passe" name="password" ">
+                    </div>
+                    <a href="#" class="forgot_mdp">Mot de passe oublié ?</a>
+                    <input type="submit" class="gray_btn" name="submit" value="Valider">
+                </div>
+            </form>
+        </div>
+        <a href="#" class="red_btn">Google +</a>
+    </div>
+
+</main>
+</body>
+</html>
