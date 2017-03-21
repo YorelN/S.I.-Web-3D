a1f4559701b512ec54bf25bda35bf29c7eb1bca7
Nicolas LEROY <nicolas.leroy@hetic.net>
2017-03-21 13:44:58 +0100

    Ajout intégration login et accueil

---
+++ Integration/home.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../S.I.-Web-3D/assets/styles/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+    <title>Accueil</title>
+</head>
+<body id="home">
+
+<div>
+    <h1>Medecine_App</h1>
+    <div class="category">Anatomie</div>
+    <div class="category">Rechercher</div>
+    <div class="category">Favoris</div>
+    <div class="category">Articles</div>
+    <div class="category">Organisation</div>
+</div>
+
+</body>
+</html>
