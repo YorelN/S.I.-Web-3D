88b9a841460386d75a56be7960d3986188152589
AmFlint <amasselot@hotmail.com>
2017-03-21 14:40:08 +0100

    debut de l'intégration

--- views/main.php
+++ views/main.php
@@ -1,9 +1,32 @@
-<h1>Hello world</h1>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/styles/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+    <title>Accueil</title>
+</head>
+<body id="home">
+
+<div>
+    <h1>Medecine_App</h1>
+    <div class="category">Anatomie</div>
+    <div class="category">Rechercher</div>
+    <div class="category">Favoris</div>
+    <div class="category">Articles</div>
+    <div class="category">Organisation</div>
+</div>
+
+</body>
+</html>
 
 <?php
-foreach ($viewmodel as $item): ?>
-    <div>
-        <h2><?=$item['name']?></h2>
-        <p><?=$item['contenu']?></p>
-    </div>
-<?php endforeach; ?>
+//foreach ($viewmodel as $item): ?>
+<!--    <div>-->
+<!--        <h2>--><?//=$item['name']?><!--</h2>-->
+<!--        <p>--><?//=$item['contenu']?><!--</p>-->
+<!--    </div>-->
+<?php //endforeach; ?>
