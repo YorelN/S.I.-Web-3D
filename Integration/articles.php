5c59d79cfc4bd06a6580e409565b9900946b3bc3
Nicolas LEROY <nicolas.leroy@hetic.net>
2017-03-22 11:33:53 +0100

    Ajout intégration suite

---
+++ Integration/articles.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../S.I.-Web-3D/assets/styles/main.css">
+
+    <title>Articles</title>
+</head>
+<body id="login">
+<?php require "nav_bar.php"?>
+
+    <nav class="clearfix">
+        <a href="#" class="all_articles">
+            <p class="active">Articles récents</p>
+        </a>
+        <a href="#" class="all_articles">
+            <p class="notActive">Articles conseillé</p>
+        </a>
+    </nav>
+
+
+    <section>
+        <article class="article">
+            <h2>Lorem Ipsum</h2>
+            <span>Vendredi 13 Avril 2017 - 16:20</span>
+            <p>When you’re sick, listen to your mother: go see your doctor and find out what illness you have and how to get better. If you’re…</p>
+            <address>Auteur: Ada Lovelace </address>
+            <i><img src="../Pictos/star.svg" alt=""></i>
+        </article>
+
+        <article class="article">
+            <h2>Lorem Ipsum</h2>
+            <span>Vendredi 13 Avril 2017 - 16:20</span>
+            <p>When you’re sick, listen to your mother: go see your doctor and find out what illness you have and how to get better. If you’re…</p>
+            <address>Auteur: Ada Lovelace </address>
+            <i><img src="../Pictos/unstar.svg" alt=""></i>
+        </article>
+
+        <article class="article">
+            <h2>Lorem Ipsum</h2>
+            <span>Vendredi 13 Avril 2017 - 16:20</span>
+            <p>When you’re sick, listen to your mother: go see your doctor and find out what illness you have and how to get better. If you’re…</p>
+            <address>Auteur: Ada Lovelace </address>
+            <i><img src="../Pictos/star.svg" alt=""></i>
+        </article>
+
+        <article class="article">
+            <h2>Lorem Ipsum</h2>
+            <span>Vendredi 13 Avril 2017 - 16:20</span>
+            <p>When you’re sick, listen to your mother: go see your doctor and find out what illness you have and how to get better. If you’re…</p>
+            <address>Auteur: Ada Lovelace </address>
+            <i><img src="../Pictos/unstar.svg" alt=""></i>
+        </article>
+
+        <article class="article">
+            <h2>Lorem Ipsum</h2>
+            <span>Vendredi 13 Avril 2017 - 16:20</span>
+            <p>When you’re sick, listen to your mother: go see your doctor and find out what illness you have and how to get better. If you’re…</p>
+            <address>Auteur: Ada Lovelace </address>
+            <i><img src="../Pictos/unstar.svg" alt=""></i>
+        </article>
+
+        <article class="article">
+            <h2>Lorem Ipsum</h2>
+            <span>Vendredi 13 Avril 2017 - 16:20</span>
+            <p>When you’re sick, listen to your mother: go see your doctor and find out what illness you have and how to get better. If you’re…</p>
+            <address>Auteur: Ada Lovelace </address>
+            <i><img src="../Pictos/unstar.svg" alt=""></i>
+        </article>
+    </section>
+
+
+</body>
+</html>
