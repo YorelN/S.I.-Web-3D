f6244c299d4218e2a57a3b00a22143f7e13b5a13
Marie CHARLES <marie.charles@hetic.net>
2017-03-22 12:39:24 +0100

    essai du plugin

---
+++ EssaiPlugin/Scripts/BoneGrabStep4.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneGrabStep4 : MonoBehaviour
+{
+
+    public Vector3 Deplacement;
+    public float Speed = 10.0F;
+    public int smooth = 5;
+    public int tiltAngle = 45;
+    public float RotationInitial = 180;
+    public TextMesh ShowAnglex;
+    public TextMesh ShowAnglez;
+    public float StepRotationMin;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float RotationZ = Input.GetAxis("Horizontal") * Speed;
+        RotationZ *= Time.deltaTime;
+        transform.Rotate(-Vector3.up, RotationZ * Speed);
+
+
+        float tiltAroundZ = StepRotationMin + Input.GetAxis("Horizontal") * tiltAngle;
+        float anglerotx = Input.GetAxis("Vertical") * tiltAngle;
+        Quaternion target = Quaternion.Euler(-anglerotx, RotationInitial, tiltAroundZ);
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+
+        ShowAnglex.text = "  " + anglerotx.ToString("f0");
+        tiltAroundZ *= -1;
+        ShowAnglez.text = "  " + tiltAroundZ.ToString("f0");
+
+    }
+}
---
+++ EssaiPlugin/Scripts/BoneStepMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneStepMenu : MonoBehaviour
+{
+
+    public Vector3 Deplacement;
+    public float Speed = 10.0F;
+    public int smooth = 5;
+    public int tiltAngle = 45;
+    public float RotationInitial = 180;
+    public float StepRotationMin;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float RotationZ = Input.GetAxis("VideHorizontal") * Speed;
+        RotationZ *= Time.deltaTime;
+        transform.Rotate(-Vector3.up, RotationZ * Speed);
+
+
+        float tiltAroundZ = StepRotationMin + Input.GetAxis("VideHorizontal") * tiltAngle;
+        float anglerotx = Input.GetAxis("VideVertical") * tiltAngle;
+        Quaternion target = Quaternion.Euler(-anglerotx, RotationInitial, tiltAroundZ);
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+
+    }
+}
