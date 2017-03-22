b9e1cd394b31b6d2161136360d26592bea0f5516
Nicolas LEROY <nicolas.leroy@hetic.net>
2017-03-22 16:51:21 +0100

    Intégration suite

---
+++ Integration/details_article.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../S.I.-Web-3D/assets/styles/main.css">
+
+    <title>Détails article</title>
+</head>
+<body>
+<?php require "nav_bar.php" ?>
+
+<div class="trois_d">
+
+
+</div>
+
+<div class="add_revision">
+    <i><img src="../img-layout/Pictos/revision.svg" alt="Organisation"></i>
+    <a href="#">Planifier des révisions</a>
+</div>
+
+<section class="details_article">
+    <article>
+        <h1>Lorem Ipsum</h1>
+        <span>Vendredi 13 Avril 2017 - 16:20</span>
+        <p>Have years of smoking made your teeth yellow and darkened the color of your lips? Here are some home remedies that can help you retain teeth and lip color.
+
+            Whiter #Teeth
+
+            These home remedies will lighten the stains caused by smoking with the use of natural products. All the ingredients used are inexpensive and will save you many expensive trips to the dentist.
+
+            Strawberries.
+
+            <br>
+            <br>
+
+            Take three to six ripe strawberries, two teaspoons of baking soda, one teaspoon of cream of tartar and a cup of water. Puree the strawberries and make a paste. Apply to your teeth overnight for a week and dazzle everybody with your bright new smile.
+
+            Sugarless Gum.
+
+            Munch on a piece of sugarless gum after you’ve had your coffee and cigarette. Doing so will produce more saliva to swish away the darkening liquids before they have a chance to stain your teeth.
+
+            Water.
+            <br>
+            <br>
+
+            Sip on water or rinse you mouth every time you eat or drink a stain-producing food or beverage and after you light up. This will help cleanse your teeth and prevent stains from accumulating.
+
+            Bicarbonate of Soda.
+
+            Brush your teeth with bicarbonate of soda and then brush with your normal toothpaste. Repeat this routine every 15 days.
+
+            Don’t rush when you brush.
+
+            Concentrate on brushing your teeth slowly with circular movements using a soft bristled toothbrush. Take your time and ensure that you get to those hard to reach areas.
+
+            Lighter Lips
+
+            Smokers have darker lips than normal due to nicotine stains. Here are some methods which you can use to lighten your lips a shade.
+
+            Vaseline.
+            <br>
+            <br>
+
+            Apply Vaseline to your lips regularly. This keeps lips moisturized and helps prevent staining of your lips.
+
+            Lemon and Glycerin.
+
+            Scrubbing your lips gently with lemon daily should help remove the discoloration, but this may be harsh on your lips, so make sure you apply glycerin to your lips first. Leave the lemon and glycerin on for at least ten minutes before washing off. You could also do the same at night, and sleep with lemon on your lips.
+
+            Almond Oil.
+            <br>
+            <br>
+
+            Apply almond oil with a dab of cream on your lips. This will ensure that your lips are moisturized and the layer of oil will help prevent build up of nicotine stains on your lips. You can also apply almond oil or cream of milk daily on the lips.
+
+            The most important remedy to reduce staining on your lips and teeth is to quit smoking. The efficiency of these home remedies is dramatically increased if you do not add to the nicotine staining every day.
+
+            Warning: The reader of this article should exercise all precautionary measures while following instructions on the home remedies from this article. Avoid using any of these products if you are allergic to it. The responsibility lies with the reader and not with the site or the writer.</p>
+        <address>Auteur: Ada Lovelace </address>
+    </article>
+</section>
+
+<aside>
+    <article class="article_lies">
+        <h2>Lorem Ipsum</h2>
+        <span>Vendredi 13 Avril 2017 - 16:20</span>
+        <p>When you’re sick, listen to your mother: go see your doctor and find out what illness you have and how to get better. If you’re…</p>
+        <address>Auteur: Ada Lovelace </address>
+        <i><img src="../img-layout/Pictos/star.svg" alt=""></i>
+    </article>
+
+    <article class="article_lies">
+        <h2>Lorem Ipsum</h2>
+        <span>Vendredi 13 Avril 2017 - 16:20</span>
+        <p>When you’re sick, listen to your mother: go see your doctor and find out what illness you have and how to get better. If you’re…</p>
+        <address>Auteur: Ada Lovelace </address>
+        <i><img src="../img-layout/Pictos/star.svg" alt=""></i>
+    </article>
+
+
+</aside>
---
+++ Integration/trois_d_fullpage.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../S.I.-Web-3D/assets/styles/main.css">
+    <title>3D Full Page</title>
+<body>
+<?php require "./nav_bar.php" ?>
+
+
+<div class="trois_d_fullpage">
+</div>
+
+</body>
+</html>
