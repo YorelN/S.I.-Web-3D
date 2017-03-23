487e919e5e33a4b6b8665dea7a28ff6300366f91
YorelN <nicolas.leroy971@hetic.net>
2017-03-23 14:40:12 +0100

    maj css hover + ajout a propos

---
+++ Integration/a_propos.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../assets/styles/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+
+    <title>Login</title>
+</head>
+<body id="login">
+
+
+    <section id="a_propos">
+        <div><img src="../assets/img-layout/Pictos/doctorapp.svg" alt=""></div>
+        <h4>Doctor'App, la solution #1 pour les futurs professionnels de la santé</h4>
+        <p>Parcourez de nombreux cours accompagné d'une modélisation en 3D inédite</p>
+        <div class="link">
+            <a href="" class="blue_btn">Inscription</a>
+            <a href="" class="green_btn">Connexion</a>
+        </div>
+    </section>
+
+
+</body>
+</html>
