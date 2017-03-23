e3a5f4dfac07ad9cf51706707950566c2bcb34b7
AmFlint <amasselot@hotmail.com>
2017-03-23 20:49:14 +0100

    error 404

--- views/main.php
+++ views/main.php
@@ -3,12 +3,11 @@
 <head>
     <meta charset="utf-8">
     <title>Page not found 404</title>
-    <link rel="stylesheet" href="assets/styles/main.css">
+    <link rel="stylesheet" href="../assets/styles/main.css">
 </head>
-<body>
-<?php require "includes/nav_bar.php"?>
+<body class="lol">
     <h1 class="oops">Oops... Cette page n'existe pas!</h1>
-    <a href="home" class="tohome">Retourner à l'acceuil</a>
-    <img src="assets/img-layout/404.png" alt="" class="notfound">
+    <a href="<?=ROOT_URL?>" class="blue_tag">Retourner à l'acceuil</a>
+    <img src="../assets/img-layout/404.png" alt="" class="notfound">
 </body>
 </html>
