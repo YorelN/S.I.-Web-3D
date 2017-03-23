cfd6504de332e4834d4aff41fddec223ab985f24
YorelN <nicolas.leroy971@hetic.net>
2017-03-23 10:05:42 +0100

    Maj intégration

--- Integration/subscribe.php
+++ Integration/subscribe.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../S.I.-Web-3D/assets/styles/main.css">
+    <link rel="stylesheet" href="../assets/styles/main.css">
 
     <title>Inscription</title>
 </head>
